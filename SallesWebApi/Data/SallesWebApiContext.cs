d202e362d2ef25aecc99e55eadfb8685d4cf8ad0
Dalton26-front <dtcastro2013@gmail.com>
2022-03-02 17:44:56 -0300

    CRUD Scaffolding

---
+++ SallesWebApi/Data/SallesWebApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SallesWebApi.Models;
+
+namespace SallesWebApi.Data
+{
+    public class SallesWebApiContext : DbContext
+    {
+        public SallesWebApiContext (DbContextOptions<SallesWebApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SallesWebApi.Models.Department> Department { get; set; }
+    }
+}
